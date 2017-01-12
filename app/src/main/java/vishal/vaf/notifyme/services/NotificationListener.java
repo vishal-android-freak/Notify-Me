ff010dd7bdf726c43f3d04fe257bcb0441fbb1d8
Vishal Dubey <yzvishal.vd@gmail.com>
2017-01-13 00:25:02 +0530

    Implement Notify Mode

--- app/src/main/java/vishal/vaf/notifyme/services/NotificationListener.java
+++ app/src/main/java/vishal/vaf/notifyme/services/NotificationListener.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * <p>
+ *
  * Copyright (c) 2017 Vishal Dubey
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,8 +38,24 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import vishal.vaf.notifyme.model.NotificationModel;
 
 public class NotificationListener extends NotificationListenerService {
 
@@ -59,8 +75,14 @@ public class NotificationListener extends NotificationListenerService {
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+
     private boolean isWhatsAppEnabled = false, isAssistEnabled = false, isNotifyEnabled = false, isFbMsgEnabled = false;
+
     private EnableToggleReceiver receiver;
+
+    private MqttAsyncClient client;
+
+    private HashMap<String, NotificationModel> hashMap = new HashMap<>();
 
     public NotificationListener() {
 
@@ -103,6 +125,17 @@ public class NotificationListener extends NotificationListenerService {
         Log.d(TAG, "connected");
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         editor = sharedPreferences.edit();
+        isAssistEnabled = sharedPreferences.getBoolean(isAssistOn, false);
+        isNotifyEnabled = sharedPreferences.getBoolean(isNotifyOn, false);
+        isWhatsAppEnabled = sharedPreferences.getBoolean(isWhatsAppOn, false);
+        isFbMsgEnabled = sharedPreferences.getBoolean(isFbMsgOn, false);
+        try {
+            if (isNotifyEnabled) {
+                setupMqtt();
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -110,6 +143,11 @@ public class NotificationListener extends NotificationListenerService {
         Log.d(TAG, "disconnected");
         fbMsgList.clear();
         waList.clear();
+        try {
+            client.disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     private void handleAssistBotNotifications(StatusBarNotification statusBarNotification) {
@@ -123,50 +161,85 @@ public class NotificationListener extends NotificationListenerService {
 
         String packageName = statusBarNotification.getPackageName();
 
-        for (String key : bundle.keySet()) {
-            if (key.equals("android.title")) {
-                if (packageName.equals("com.whatsapp")) {
-                    if (!waList.contains(bundle.getString(key))) {
-                        senderName = bundle.getString(key);
-                        List<Notification.Action> actions = extender.getActions();
-                        for (Notification.Action act : actions) {
-                            if (act != null && act.getRemoteInputs() != null) {
-                                remoteInputs = act.getRemoteInputs();
-                                pendingIntent = act.actionIntent;
-                            }
-                        }
+        if (packageName.equals("com.whatsapp")) {
+            if (!waList.contains(bundle.getString(Notification.EXTRA_TITLE))) {
+                senderName = bundle.getString(Notification.EXTRA_TITLE);
+                List<Notification.Action> actions = extender.getActions();
+                for (Notification.Action act : actions) {
+                    if (act != null && act.getRemoteInputs() != null) {
+                        remoteInputs = act.getRemoteInputs();
+                        pendingIntent = act.actionIntent;
                     }
-                } else if (packageName.equals("com.facebook.orca")) {
-                    if (!fbMsgList.contains(bundle.getString(key))) {
-                        senderName = bundle.getString(key);
-                        List<Notification.Action> actions = extender.getActions();
-                        for (Notification.Action act : actions) {
-                            if (act != null && act.getRemoteInputs() != null) {
-                                remoteInputs = act.getRemoteInputs();
-                                pendingIntent = act.actionIntent;
-                            }
-                        }
+                }
+            }
+        } else if (packageName.equals("com.facebook.orca")) {
+            if (!fbMsgList.contains(bundle.getString(Notification.EXTRA_TITLE))) {
+                senderName = bundle.getString(Notification.EXTRA_TITLE);
+                List<Notification.Action> actions = extender.getActions();
+                for (Notification.Action act : actions) {
+                    if (act != null && act.getRemoteInputs() != null) {
+                        remoteInputs = act.getRemoteInputs();
+                        pendingIntent = act.actionIntent;
                     }
                 }
             }
         }
         if (isWhatsAppEnabled) {
             if (packageName.equals("com.whatsapp"))
-                reply(remoteInputs, pendingIntent, bundle, senderName, packageName);
+                reply(remoteInputs, pendingIntent, bundle, senderName, packageName, null, null);
         }
 
         if (isFbMsgEnabled) {
             if (packageName.equals("com.facebook.orca"))
-                reply(remoteInputs, pendingIntent, bundle, senderName, packageName);
+                reply(remoteInputs, pendingIntent, bundle, senderName, packageName, null, null);
         }
 
     }
 
     private void handleNotifyNotifications(StatusBarNotification statusBarNotification) {
+        Bundle bundle = statusBarNotification.getNotification().extras;
+        RemoteInput[] remoteInputs = null;
+        PendingIntent pendingIntent = null;
+
+        Notification.WearableExtender extender = new Notification.WearableExtender(statusBarNotification.getNotification());
+        List<Notification.Action> actions = extender.getActions();
+        for (Notification.Action act : actions) {
+            if (act != null && act.getRemoteInputs() != null) {
+                remoteInputs = act.getRemoteInputs();
+                pendingIntent = act.actionIntent;
+            }
+        }
+        if (actions.size() > 0) {
+            try {
+                JSONObject object = new JSONObject();
+                object.put("name", bundle.getString(Notification.EXTRA_TITLE));
+                object.put("message", bundle.getString(Notification.EXTRA_TEXT));
+                object.put("id", statusBarNotification.getKey());
+                object.put("app_name", statusBarNotification.getPackageName());
+
+                MqttMessage mqttMessage = new MqttMessage(object.toString().getBytes());
+                mqttMessage.setRetained(false);
+                mqttMessage.setQos(1);
+
+                if (client.isConnected()) {
+                    client.publish("test", mqttMessage);
+                    hashMap.put(statusBarNotification.getKey(), new NotificationModel(bundle, pendingIntent, remoteInputs));
+                    Log.d(TAG, "pushed");
+                } else {
+                    if (client != null)
+                        client.reconnect();
+                    else
+                        setupMqtt();
+                }
+
+            } catch (JSONException | MqttException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
-    private void reply(RemoteInput[] remoteInputs, PendingIntent pendingIntent, Bundle bundle, String senderName, String packageName) {
+    private void reply(RemoteInput[] remoteInputs, PendingIntent pendingIntent, Bundle bundle, String senderName, String packageName, String message, String notifId) {
 
         if (remoteInputs == null || pendingIntent == null)
             return;
@@ -176,7 +249,7 @@ public class NotificationListener extends NotificationListenerService {
 
         if (packageName.equals("com.whatsapp")) {
             for (RemoteInput remoteInput : remoteInputs) {
-                bundle.putCharSequence(remoteInput.getResultKey(), sharedPreferences.getString("assist_wa_msg", ""));
+                bundle.putCharSequence(remoteInput.getResultKey(), message == null ? sharedPreferences.getString("assist_wa_msg", "") : message);
             }
         } else if (packageName.equals("com.facebook.orca")) {
             for (RemoteInput remoteInput : remoteInputs) {
@@ -187,10 +260,13 @@ public class NotificationListener extends NotificationListenerService {
         RemoteInput.addResultsToIntent(remoteInputs, localIntent, bundle);
         try {
             pendingIntent.send(this, 0, localIntent);
-            if (packageName.equals("com.whatsapp")) {
+            if (packageName.equals("com.whatsapp") && !isNotifyEnabled) {
                 waList.add(senderName);
-            } else if (packageName.equals("com.facebook.orca")) {
+            } else if (packageName.equals("com.facebook.orca") && !isNotifyEnabled) {
                 fbMsgList.add(senderName);
+            }
+            if (isNotifyEnabled) {
+                hashMap.remove(notifId);
             }
         } catch (PendingIntent.CanceledException e) {
             Log.e("", "replyToLastNotification error: " + e.getLocalizedMessage());
@@ -209,16 +285,89 @@ public class NotificationListener extends NotificationListenerService {
                 isNotifyEnabled = intent.getStringExtra(isNotifyOn).equals("1");
                 editor.putBoolean(isNotifyOn, isNotifyEnabled);
                 Log.d(TAG, "notify " + isNotifyEnabled);
+                try {
+                    if (isNotifyEnabled) {
+                        setupMqtt();
+                    } else {
+                        client.disconnect();
+                    }
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
             } else if (intent.getAction().equals(ENABLE_WA)) {
                 isWhatsAppEnabled = intent.getStringExtra(isWhatsAppOn).equals("1");
                 editor.putBoolean(isWhatsAppOn, isWhatsAppEnabled);
                 Log.d(TAG, "wa " + isWhatsAppEnabled);
+                if (!isWhatsAppEnabled)
+                    waList.clear();
             } else if (intent.getAction().equals(ENABLE_FB_MSG)) {
                 isFbMsgEnabled = intent.getStringExtra(isFbMsgOn).equals("1");
                 editor.putBoolean(isFbMsgOn, isFbMsgEnabled);
                 Log.d(TAG, "fbmsg " + isFbMsgEnabled);
+                if (!isFbMsgEnabled)
+                    fbMsgList.clear();
             }
             editor.apply();
         }
+    }
+
+    private void setupMqtt() throws MqttException {
+        MemoryPersistence persistence = new MemoryPersistence();
+        client = new MqttAsyncClient("tcp://test.org", "android_phone", persistence);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(false);
+        options.setAutomaticReconnect(true);
+        options.setUserName("vishal");
+        options.setPassword("vishal123".toCharArray());
+        options.setConnectionTimeout(0);
+        client.connect(options, new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                Log.d(TAG, "mqtt connected");
+                try {
+                    client.subscribe("test1", 1);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                exception.printStackTrace();
+                try {
+                    client.reconnect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                cause.printStackTrace();
+                try {
+                    client.reconnect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d(TAG, message.toString());
+                JSONObject object = new JSONObject(message.toString());
+                String appName = object.optString("app_name");
+                String senderName = object.optString("name");
+                String senderMsg = object.optString("message");
+                String id = object.optString("id");
+
+                reply(hashMap.get(id).getRemoteInputs(), hashMap.get(id).getPendingIntent(), hashMap.get(id).getBundle(), senderName, appName, senderMsg, id);
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
     }
 }
